f4607d0bcf33806b4177fba76ce94c33eb5d4abc
SKTT1Ryze <linuxgnulover@gmail.com>
2021-06-06 01:24:45 +0800

    continue

--- src/frontend/scene.rs
+++ src/frontend/scene.rs
@@ -1,22 +1,25 @@
 //! 场景的统一抽象
 
 use std::marker::PhantomData;
-
-use bevy_egui::egui::{CtxRef, InnerResponse, Ui};
+use bevy_egui::egui::{self, CtxRef, InnerResponse};
+use super::state::StateMachine;
+use crate::config::*;
 
 #[derive(Default)]
-pub struct Scene<F: Fn(&CtxRef) -> InnerResponse<()>>
+pub struct Scene<F: Fn(&CtxRef, &mut StateMachine) -> InnerResponse<R>, R>
 {
-    f: F,
+    show_f: F,
+    _maker: PhantomData<R>
 }
 
-impl<F> Scene<F>
+impl<F, R> Scene<F, R>
 where
-    F: Fn(&CtxRef) -> InnerResponse<()>
+    F: Fn(&CtxRef, &mut StateMachine) -> InnerResponse<R>
 {
     pub fn new(f: F) -> Self {
         Self {
-            f,
+            show_f: f,
+            _maker: PhantomData
         }
     }
 }
@@ -24,6 +27,27 @@ where
 #[derive(Default)]
 pub struct Scenes
 {
-    scenes: Vec<Scene<fn(&CtxRef)->InnerResponse<()>>>
+    scenes: Vec<Scene<fn(&CtxRef, &mut StateMachine)->InnerResponse<()>, ()>>
 }
 
+impl Scenes {
+    pub fn add_scene(
+        &mut self,
+    ) {
+        fn f(ctx: &CtxRef, state_machine: &mut StateMachine) -> InnerResponse<()> {
+            egui::SidePanel::left("side_panel", SIDE_PANEL_WIDTH)
+                .show(ctx, |ui| {
+                    ui.heading("Side Panel");
+                    let mut input = String::new();
+                    ui.horizontal(|ui| {
+                        ui.label("input box: ");
+                        ui.text_edit_singleline(&mut input);
+                    });
+                    if ui.add(egui::Button::new("button")).clicked() {
+                        println!("button is cliked!");
+                    }
+                })
+        };
+        self.scenes.push(Scene::new(f));
+    }
+}
--- src/frontend/state.rs
+++ src/frontend/state.rs
@@ -1,1 +1,3 @@
 //! 前端 UI 界面之状态机实现(用于场景变换你)
+
+pub struct StateMachine;
